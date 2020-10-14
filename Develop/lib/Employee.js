c6180e9ea90c7ba7dab84754416026971ae209fb
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-10-13 17:50:20 -0700

    all tests pass

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,15 +1,5 @@
 // TODO: Write code to define and export the Employee class
 class Employee{
-    // constructor(){
-    //     //create a new employee
-    // }
-    // constructor(name){
-    //     this.name = name;
-    // }
-    // constructor(name, id){
-    //     this.name = name;
-    //     this.id = id;
-    // }
     constructor(name, id, email){
         this.name = name;
         this.id = id;
